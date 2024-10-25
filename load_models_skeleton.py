0b2fd95faa3129c4a225d33fc037f884d3e98376
Andreas Olsen <andreas.flensted98@gmail.com>
2024-10-25 16:33:06 +0200

    refactored model loading

--- load_models_skeleton.py
+++ load_models_skeleton.py
@@ -1,7 +1,18 @@
 import torch
 from transformers import AutoTokenizer, AutoModel
 
-def get_embeddings(dna, model, tokenizer, mean_pool=True):
+def get_embeddings(dna:str, model:AutoModel, tokenizer:AutoTokenizer, mean_pool:bool=True) -> torch.Tensor:
+    """Get embeddings for a DNA sequence given a model and tokenizer
+
+    Args:
+        dna (str): The DNA Sequence
+        model (AutoModel): The loaded huggingface model
+        tokenizer (AutoTokenizer): The loaded huggingface tokenizer
+        mean_pool (bool, optional): Pooling type. Defaults to True.
+
+    Returns:
+        torch.Tensor: The embeddings for the DNA sequence
+    """
     inputs = tokenizer(dna, return_tensors = 'pt')["input_ids"]
     hidden_states = model(inputs)[0]
     if mean_pool:
@@ -10,20 +21,25 @@ def get_embeddings(dna, model, tokenizer, mean_pool=True):
         embedding = torch.max(hidden_states[0], dim=0)
     return embedding
 
-def load_model(model_path):
-    return AutoModel.from_pretrained(model_path, trust_remote_code=True)
+def load_model_and_tokenizer(model_path:str, tokenizer_path) -> AutoModel:
+    """Loads the model and tokenizer from huggingface
+    Args:
+        model_path (str): The model path
+        tokenizer_path (str): The tokenizer path
 
-def load_tokenizer(tokenizer_path):
-    return  AutoTokenizer.from_pretrained(tokenizer_path, trust_remote_code=True)
-
+    Returns:
+        AutoModel: The loaded model
+        AutoTokenizer: The loaded tokenizer
+    """
+    model = AutoModel.from_pretrained(model_path, trust_remote_code=True)
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
+    return model, tokenizer
 
 if __name__ == "__main__":
     dna = "ATCG"
     model_path = "zhihan1996/DNABERT-S"
     tokenizer_path = "zhihan1996/DNABERT-S"
 
-    model = load_model(model_path)
-    tokenizer = load_tokenizer(tokenizer_path)
+    model, tokenizer = load_model_and_tokenizer(model_path, tokenizer_path)
 
     embeddings = get_embeddings(dna, model, tokenizer)
-    print(embeddings)
