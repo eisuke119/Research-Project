b995f25813473c158d80f9a5965ee468420db275
ah140797 <ah140797@gmail.com>
2024-10-27 17:20:14 +0100

    kk

--- utils/main_test.py
+++ utils/main_test.py
@@ -10,20 +10,14 @@ from utils import (
 )
 
 
-def main(args):
+def main():
+    file_path = "metahit_data/contigs.fna.gz"
 
-    labels, dna_sequences = load_contigs(args.data_dir)
+    labels, dna_sequences = load_contigs(file_path)
 
     print(len(labels))
     print(len(dna_sequences))
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Evaluate clustering")
-    # parser.add_argument('--test_model_dir', type=str, default="/root/trained_model", help='Directory to save trained models to test')
-    # parser.add_argument('--model_list', type=str, default="test", help='List of models to evaluate, separated by comma. Currently support [tnf, tnf-k, dnabert2, hyenadna, nt, test]')
-    parser.add_argument(
-        "--data_dir", type=str, default="/root/data", help="Data directory"
-    )
-    args = parser.parse_args()
-    main(args)
+    main()
