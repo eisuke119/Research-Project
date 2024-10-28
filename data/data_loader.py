5cc000d3c97632716b55d5d1754f7fe7a92b68f2
Andreas Olsen <andreas.flensted98@gmail.com>
2024-10-28 08:55:24 +0100

    added docstring for custom dataloader

--- data/data_loader.py
+++ data/data_loader.py
@@ -5,14 +5,40 @@ import pandas as pd
 
 
 class DNADataset(Dataset):
-    def __init__(self, csv_file, tokenizer):
+    """Custom Dataset for DNA Sequences
+
+    Args:
+        Dataset (Dataset): Pytorch Dataset
+    """
+
+    def __init__(self, csv_file: str, tokenizer: object):
+        """_summary_
+
+        Args:
+            csv_file (str): Filepath to contig csv
+            tokenizer (object): Model Tokenizer
+        """
         self.data = pd.read_csv(csv_file)
         self.tokenizer = tokenizer
 
-    def __len__(self):
+    def __len__(self) -> int:
+        """Get length of the dataset
+
+        Returns:
+            int: N
+        """
         return len(self.data)
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx: int) -> tuple[object, str]:
+        """Retrieve item from dataset
+
+        Args:
+            idx (int): _description_
+
+        Returns:
+            tuple[object, str]: DNA embeddings, Species
+        """
+
         dna = self.data.iloc[idx]["sequence"]
         label = self.data.iloc[idx]["species"]
         inputs = self.tokenizer(dna, return_tensors="pt")["input_ids"].squeeze(0)
