8281693bbfa17e604ac9a1e5aaf9a2af25a87371
Andreas Olsen <andreas.flensted98@gmail.com>
2024-11-08 18:40:11 +0100

    testing pipeline (PROKBERT): segmementation params to control max length corrected key

--- src/embeddings.py
+++ src/embeddings.py
@@ -126,7 +126,7 @@ def calculate_llm_embedding(dna_sequences, batch_size, model_name, model_path):
     print(f"Using device: {device}\nwith {n_gpu} GPUs")
     if model_name == "ProkBERT":
         tokenization_parameters = {"kmer": 6, "shift": 2}
-        sequence_length_parameters = {"max": 2048}
+        sequence_length_parameters = {"max_length": 2048}
         tokenizer = ProkBERTTokenizer(
             tokenization_params=tokenization_parameters,
             operation_space="sequence",
