6e98ea3a68ef3271ee20c868c2f20be6b40c895d
Andreas Olsen <andreas.flensted98@gmail.com>
2024-11-14 12:56:30 +0100

    debug (kmedoid): changed to block calculation removed imports

--- src/eval.py
+++ src/eval.py
@@ -1,11 +1,11 @@
 import os
 
-import tables as tb
+# import tables as tb
 import numpy as np
 from scipy.optimize import linear_sum_assignment
 import sklearn.metrics
 
-from .utils import calculate_similarity_matrix
+# from .utils import calculate_similarity_matrix
 import tqdm
 
 
